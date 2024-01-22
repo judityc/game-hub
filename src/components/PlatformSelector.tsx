fe7a64b373cccc4a7e27757127e303232059040a
judityc <judityc@gmail.com>
2024-01-22 11:39:36 +0200

    Build platform selector

---
+++ src/components/PlatformSelector.tsx
@@ -0,0 +1,24 @@
+import { Button, Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/react";
+import { BsChevronDown } from "react-icons/bs";
+import usePlatforms from "../hooks/usePlatforms";
+
+const PlatformSelector = () => {
+  const { data, error } = usePlatforms();
+
+  if (error) return null;
+  
+  return (
+    <Menu>
+      <MenuButton as={Button} rightIcon={<BsChevronDown />}>
+        Platfoms
+      </MenuButton>
+      <MenuList>
+        {data.map((paltform) => (
+          <MenuItem key={paltform.id}>{paltform.name}</MenuItem>
+        ))}
+      </MenuList>
+    </Menu>
+  );
+};
+
+export default PlatformSelector;
